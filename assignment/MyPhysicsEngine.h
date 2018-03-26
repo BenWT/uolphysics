1c1c6a42c5e0d5f28dfb4fea48c19add77fa3ce7
Ben Townshend <bentownshend@icloud.com>
2018-03-26 12:40:32 +0100

    added to course layout

--- assignment/MyPhysicsEngine.h
+++ assignment/MyPhysicsEngine.h
@@ -13,6 +13,61 @@ namespace PhysicsEngine
 	static const PxVec3 color_palette[] = {PxVec3(46.f/255.f,9.f/255.f,39.f/255.f),PxVec3(217.f/255.f,0.f/255.f,0.f/255.f),
 		PxVec3(255.f/255.f,45.f/255.f,0.f/255.f),PxVec3(255.f/255.f,140.f/255.f,54.f/255.f),PxVec3(4.f/255.f,117.f/255.f,111.f/255.f)};
 
+
+	// Positive X is to the right
+	// Positive Z is towards the camera
+	const std::vector<PxVec3> courseVerts = {
+		PxVec3(3, 0, 3),
+		PxVec3(-3, 0, 3),
+		PxVec3(-3, 0, -3),
+		PxVec3(3, 0, -3),
+		PxVec3(3, 1, 3),
+		PxVec3(-3, 1, 3),
+		PxVec3(-3, 1, -3),
+		PxVec3(3, 1, -3),
+
+		PxVec3(4, 0, 4),
+		PxVec3(-4, 0, 4),
+		PxVec3(-4, 0, -4),
+		PxVec3(4, 0, -4),
+		PxVec3(4, 1, 4),
+		PxVec3(-4, 1, 4),
+		PxVec3(-4, 1, -4),
+		PxVec3(4, 1, -4),
+
+	};
+	const std::vector<PxU32> courseTrigs = {
+		// Inner Set
+		5, 4, 0,
+		0, 1, 5,
+		4, 7, 3,
+		3, 0, 4,
+		7, 6, 2,
+		2, 3, 7,
+		6, 5, 1,
+		1, 2, 6,
+
+		// Outer Set
+		12, 13, 9,
+		9, 8, 12,
+		15, 12, 8,
+		8, 11, 15,
+		14, 15, 11,
+		11, 10, 14,
+		13, 14, 10,
+		10, 9, 13,
+
+		// Top Set
+		4, 5, 13,
+		13, 12, 4,
+		7, 4, 12,
+		12, 15, 7,
+		6, 7, 15,
+		15, 14, 6,
+		5, 6, 14,
+		14, 13, 5
+	};
+
 	struct FilterGroup
 	{
 		enum Enum
@@ -123,6 +178,7 @@ namespace PhysicsEngine
 		Plane* plane;
 		Sphere* ball;
 		Box* box1, box2;
+		TriangleMesh* courseMesh;
 		MySimulationEventCallback* my_callback;
 		
 	public:
@@ -145,6 +201,9 @@ namespace PhysicsEngine
 			my_callback = new MySimulationEventCallback();
 			px_scene->setSimulationEventCallback(my_callback);
 
+			courseMesh = new TriangleMesh(courseVerts, courseTrigs);
+			Add(courseMesh);
+
 			plane = new Plane();
 			plane->Color(PxVec3(210.f/255.f,210.f/255.f,210.f/255.f));
 			Add(plane);
@@ -164,7 +223,6 @@ namespace PhysicsEngine
 		virtual void CustomUpdate() 
 		{
 			// camera follows the ball here
-
 		}
 
 		/// An example use of key release handling
